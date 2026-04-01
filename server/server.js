0612244e4d282cd3a89434d08b4f53df96620f40
troytoews69-code <troytoews69@gmail.com>
2026-03-31 17:58:03 -0700

    chore: run app on port 5003

--- server/server.js
+++ server/server.js
@@ -96,5 +96,5 @@ app.post("/api/books/:id/reviews", (req, res) => {
   res.status(201).json(newReview);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
